6e5ecf5a352d29346fe5c1bb6ff6ada752c4022f
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-10-14 09:34:32 +0100

    [setup.py] set description, author and email strings

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(name="ufonormalizer",
       version="0.1",
-      description="Example implementations of various portions of the UFO Specification.",
-      author="Unified Font Object",
+      description="Example implementation of a UFO normalizer",
+      author="Tal Leming",
+      email="tal@typesupply.com",
       url="https://github.com/unified-font-object/ufo-example-implementations",
       package_dir={"": "normalization"},
       py_modules=['ufonormalizer'],
