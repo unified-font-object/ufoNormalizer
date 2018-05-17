a4487e48e090a4bfca2f2d13862a7a84b3e6de8d
Nikolaus Waxweiler <madigens@gmail.com>
2018-05-17 22:54:53 +0100

    Add empty string to test_normalizeGLIF_lib_defined data

--- tests/test_ufonormalizer.py
+++ tests/test_ufonormalizer.py
@@ -753,6 +753,8 @@ class UFONormalizerTest(unittest.TestCase):
             <dict>
                 <key>foo</key>
                 <string>bar</string>
+                <key>abc</key>
+                <string></string>
             </dict>
         </lib>
         '''.strip()
@@ -761,8 +763,10 @@ class UFONormalizerTest(unittest.TestCase):
         _normalizeGlifLib(element, writer)
         self.assertEqual(
             writer.getText(),
-            '<lib>\n\t<dict>\n\t\t<key>foo</key>\n\t\t<string>bar</string>'
-            '\n\t</dict>\n</lib>')
+            '<lib>\n\t<dict>\n'
+            '\t\t<key>abc</key>\n\t\t<string></string>\n'
+            '\t\t<key>foo</key>\n\t\t<string>bar</string>\n'
+            '\t</dict>\n</lib>')
 
     def test_normalizeGLIF_lib_undefined(self):
         element = ET.fromstring("<lib></lib>")
