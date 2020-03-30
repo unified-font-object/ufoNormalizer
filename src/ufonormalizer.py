8f1ab1f3ab830e7732f1a9dcc084e30ebb8f09fc
Cosimo Lupo <clupo@google.com>
2020-03-30 13:35:26 +0100

    Bump version: 0.3.7.dev0 → 0.4.0

--- src/ufonormalizer.py
+++ src/ufonormalizer.py
@@ -21,7 +21,7 @@ import logging
 - things that need to be improved are marked with "# TO DO"
 """
 
-__version__ = "0.3.7.dev0"
+__version__ = "0.4.0"
 description = """
 UFO Normalizer (version %s):
 
