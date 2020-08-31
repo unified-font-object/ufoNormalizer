f40616525dde477618d04194dd46019e00f03382
Ben Kiel <ben@typefounding.com>
2020-08-31 09:26:58 -0500

    Bump version: 0.4.1 → 0.5.0.dev0

--- src/ufonormalizer.py
+++ src/ufonormalizer.py
@@ -22,7 +22,7 @@ import logging
 - things that need to be improved are marked with "# TO DO"
 """
 
-__version__ = "0.4.1"
+__version__ = "0.5.0.dev0"
 description = """
 UFO Normalizer (version %s):
 
