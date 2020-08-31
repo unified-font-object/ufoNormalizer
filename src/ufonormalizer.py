32d1c4c0a45fef21831ec50edeb88dfbeea0e151
Ben Kiel <ben@typefounding.com>
2020-08-31 09:27:12 -0500

    Revert "Bump version: 0.4.1 → 0.5.0.dev0"
    This reverts commit f40616525dde477618d04194dd46019e00f03382.

--- src/ufonormalizer.py
+++ src/ufonormalizer.py
@@ -22,7 +22,7 @@ import logging
 - things that need to be improved are marked with "# TO DO"
 """
 
-__version__ = "0.5.0.dev0"
+__version__ = "0.4.1"
 description = """
 UFO Normalizer (version %s):
 
